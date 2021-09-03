198433a0e968e84c6130233e240cf2fe74f05a87
Jack Rigan <riganjack40@gmail.com>
2021-09-03 10:28:14 -0600

    implemented styling for left container of header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,36 +9,41 @@ import {
     UserGroupIcon,
     ViewGridIcon,
 } from '@heroicons/react/solid';
-
 import {
     FlagIcon,
     PlayIcon,
     SearchIcon,
     ShoppingCartIcon,
-} from '@heroicons/react/outline'
+} from '@heroicons/react/outline';
 
 function Header() {
     return (
         <div>
             
-            {/* Left */}
-            <div>
+            {/* Left Section */}
+            <div className='flex items-center'>
                 {/* Next.js image component optimizes the image and loads faster */}
-                <h1>Hello</h1>
                 <Image src='https://links.papareact.com/5me'
                 width={40}
                 height={40}
                 layout='fixed'
                 />
-                <div>
-                    <input type='text' placeholder='Search'></input>
+            
+                <div className='flex ml-2 items-center rounded-full bg-gray-100 p-2'>
+                    <SearchIcon className='h-6 text-gray-600' />
+                    <input className='flex ml-2 items-center bg-transparent outline-none placeholder-gray-500' type='text' placeholder='Search'></input>
                 </div>
             </div>
             
-            {/* Center */}
+            {/* Center Section */}
+            
+<div className='flex justify-center flex-grow'>  {/* Flex grow lets the container grow if there is extra space available */}
+    <div>
 
+    </div>
+</div>
 
-            {/* Right */}
+            {/* Right  Section*/}
         </div>
     )
 }
