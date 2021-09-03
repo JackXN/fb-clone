00cbdcf99cd0f129807ad0099c1f651e4ab02c11
Jack Rigan <riganjack40@gmail.com>
2021-09-03 12:03:38 -0600

    implemented authentication and session state

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import Image from 'next/image'
 import HeaderIcon from './HeaderIcon';
+import { useSession, signOut  } from 'next-auth/client';
 import {
     BellIcon,
     ChatIcon,
@@ -17,7 +18,10 @@ import {
     ShoppingCartIcon,
 } from '@heroicons/react/outline';
 
+
 function Header() {
+const [session] = useSession();
+
     return (
         <div className='sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md'>
             
@@ -51,6 +55,16 @@ function Header() {
         <div className='flex items-center sm:space-x-2 justify-end'>
 
             {/* Profile  */}
+
+            <Image
+            onClick={signOut}
+            className='rounded-full cursor-pointer'
+            //Take the image of the current user with an active session state of logged in
+            src={session.user.image}
+            width='40'
+            height='40'
+            layout='fixed'
+            />
             <p className='whitespace-nowrap font-semibold pr-3'>Jack Rigan</p>
             <ViewGridIcon className='icon'/>
             <ChatIcon className='icon'/>
